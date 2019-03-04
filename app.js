7fdd637595cc437483050b3ad0c210594dfd0d5b
xaviguasch <xaviguasch@gmail.com>
2019-03-04 13:01:01 +0100

    refactored route /articles via chained route handlers

--- app.js
+++ app.js
@@ -27,41 +27,44 @@ const articleSchema = new mongoose.Schema({
 const Article = mongoose.model('Article', articleSchema)
 
 
+app.route('/articles')
 
-app.get('/articles', function (req, res) {
-    Article.find(function (err, foundArticles) {
-        if (!err) {
-            res.send(foundArticles)
-        } else {
-            res.send(err)
-        }
-    })
-})
-
-app.post('/articles', function (req, res) {
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles)
+            } else {
+                res.send(err)
+            }
+        })
     })
 
-    newArticle.save(function (err) {
-        if (!err) {
-            res.send('Successfully added a new article')
-        } else {
-            res.send(err)
-        }
-    })
-})
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        })
 
-app.delete('/articles', function (req, res) {
-    Article.deleteMany(function (err) {
-        if (!err) {
-            res.send('Successfully deleted all articles')
-        } else {
-            res.send(err)
-        }
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send('Successfully added a new article')
+            } else {
+                res.send(err)
+            }
+        })
     })
-})
+
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send('Successfully deleted all articles')
+            } else {
+                res.send(err)
+            }
+        })
+    })
+
+
 
 
 
@@ -69,3 +72,39 @@ app.delete('/articles', function (req, res) {
 app.listen(3000, function () {
     console.log("Server has started successfully");
 });
+
+
+// SAVED ARTICLES
+
+// {
+//     "_id": "5c78fdf5fec9ac1db30f54e2",
+//     "title": "REST",
+//     "content": "REST is short for REpresentational State Transfer. It's architectural style for designing APIs."
+// },
+// {
+//     "_id": "5c139771d79ac8eac11e754a",
+//     "title": "API",
+//     "content": "API stands for Application Programming Interface. It is a set of subroutine definitions, communication protocols, and tools for building software. In general terms, it is a set of clearly defined methods of communication among various components. A good API makes it easier to develop a computer program by providing all the building blocks, which are then put together by the programmer."
+// },
+// {
+//     "_id": "5c1398aad79ac8eac11e7561",
+//     "title": "Bootstrap",
+//     "content": "This is a framework developed by Twitter that contains pre-made front-end templates for web design"
+// },
+// {
+//     "_id": "5c1398ecd79ac8eac11e7567",
+//     "title": "DOM",
+//     "content": "The Document Object Model is like an API for interacting with our HTML"
+// },
+// {
+//     "_id": "5c7d0ce645234762a96419ef",
+//     "title": "Jack Bauer",
+//     "content": "Jack Bauer once stepped into quicksand.",
+//     "__v": 0
+// },
+// {
+//     "_id": "5c7d0e076ce6d06582eb1e35",
+//     "title": "Michael Mayers",
+//     "content": "The new movie is quite good",
+//     "__v": 0
+// }

97ffd11d61d8814aea193418198bb3a00b640083
xaviguasch <xaviguasch@gmail.com>
2019-03-04 17:22:44 +0100

    added the delete option through the params of the request

--- app.js
+++ app.js
@@ -117,6 +117,18 @@ app.route('/articles/:articleTitle')
         })
     })
 
+    .delete(function (req, res) {
+        Article.deleteOne({
+            title: req.params.articleTitle
+        }, function (err) {
+            if (!err) {
+                res.send('Successfully deleted the corresponding article')
+            } else {
+                res.send(err)
+            }
+        })
+    })
+
 
 
 
